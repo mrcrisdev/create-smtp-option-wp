979b05d31330317fc843fd81c6469ceea8029982
Cao Quoc Bao <44928051+mrcrisdev@users.noreply.github.com>
2022-01-05 17:57:28 +0700

    Add files via upload

---
+++ smtp.php
@@ -0,0 +1,15 @@
+<?php 
+
+function send_smtp_email( $phpmailer ) {
+  $phpmailer->IsSMTP();
+  $phpmailer->Host       = "smtp.gmail.com";
+  $phpmailer->Port       = 465;
+  $phpmailer->SMTPAuth   = true;
+  $phpmailer->Username   = get_field('email_app', 'option'); //tai khoan
+  $phpmailer->Password   = get_field('password_app', 'option'); //mat khau
+  $phpmailer->SMTPSecure = "ssl";
+}
+add_action( 'phpmailer_init', 'send_smtp_email' );
+
+
+?>
